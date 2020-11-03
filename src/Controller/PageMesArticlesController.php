69b474466f3aad22e7397c1c395e368330f59b76
Léandre <delpivarleandre@gmail.com>
2020-11-03 16:52:37 +0100

    add ok

--- src/Controller/PageMesArticlesController.php
+++ src/Controller/PageMesArticlesController.php
@@ -6,6 +6,7 @@ use App\Entity\Article;
 use App\Entity\TagArticle;
 use App\Entity\Tag;
 use App\Form\ArticleType;
+use App\Repository\TagRepository;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -56,57 +57,36 @@ class PageMesArticlesController extends AbstractController
     }
 
     // CREER UN ARTICLE 
-    public function ajouterArticle(Request $resquest): Response
+    public function ajouterArticle(Request $request): Response
     {
         $article = new Article();
-        $form = $this->createForm(ArticleType::class, $article);
-        $form->handleRequest($resquest);
+        $tag_article = new TagArticle();
+        $array_post_article_tag = $_POST;
 
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($array_post_article_tag) {
+            $article->setTitre($array_post_article_tag["titre"]);
+            $article->setContenu($array_post_article_tag["contenu"]);
             $article->setIdUser($this->getUser());
             $article->setDateCrea(new DateTime());
             $article->setDateModif(new DateTime());
             $this->em->persist($article);
             $this->em->flush();
-            $this->addFlash('success', 'Votre profil a été créer avec succès');
+            $id_article = $article->getId();
+            foreach ($array_post_article_tag["tag"] as $id => $value) {
+                $tag_article->setIdArticle($id_article);
+                $tag_article->setIdTag($id);
+                $this->em->persist($tag_article);
+                $this->em->flush();
+            }
+            $this->addFlash('success', 'Votre article a été créer avec succès');
             return $this->redirectToRoute('article/voirArticles.html.twig');
         }
 
         return $this->render('article/creeArticle.html.twig', [
             'article' => $article,
             'les_tags' => $this->lesTags(),
-            'form' => $form->createView()
         ]);
     }
-
-    //EDIT L'ARTICLE CHOISI
-    public function modifierArticle(Article $article, Request $request)
-    {
-        $array_tab = $request->get('id');
-        $form = $this->createForm(ArticleType::class, $article);
-        $form->handleRequest($request);
-        // SELECT * WHERE ID USER = ID USER CONNECTÉ        
-        /** @var TagArticle $tag_article */
-        $tag_article = $this->getDoctrine()
-            ->getRepository(TagArticle::class)
-            ->findTagByArticle($array_tab);
-
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $article->setDateModif(new DateTime());
-            $this->em->flush();
-            //$this->addFlash('success', 'Modifier avec succès');
-            return $this->redirectToRoute('mes_articles');
-        }
-
-        return $this->render('article/modifierArticle.html.twig', [
-            'tag_article' => $tag_article,
-            'article' => $article,
-            'les_tags' => $this->lesTags(),
-            'form' => $form->createView()
-        ]);
-    }
-
 
     public function lesTags()
     {
@@ -115,7 +95,35 @@ class PageMesArticlesController extends AbstractController
         $les_tags = $this->getDoctrine()
             ->getRepository(Tag::class)
             ->findAll();
-
         return $les_tags;
+    }
+    //EDIT L'ARTICLE CHOISI
+    public function modifierArticle(Article $article, Request $request, TagRepository $tr)
+    {
+        $array_post_article_tag = $_POST;
+        var_dump($array_post_article_tag);
+        if ($array_post_article_tag) {
+
+            $article->setTitre($array_post_article_tag["titre"]);
+            $article->setContenu($array_post_article_tag["contenu"]);
+            $article->setDateModif(new DateTime());
+            foreach ($array_post_article_tag["tag"] as $id => $value) {
+                $tag_article = new TagArticle;
+                $tag_article->setIdArticle($article);
+                $tag_article->setIdTag($tr->findOneBy(['id' => $id]));
+                // $article->addTagArticle($tag_article);
+                $this->em->persist($tag_article);
+            }
+            $this->em->persist($article);
+            $this->em->flush();
+
+            $this->addFlash('success', 'Votre article a été modifié avec succès');
+            return $this->redirectToRoute('mes_articles');
+        }
+        var_dump($this->lesTags());
+        return $this->render('article/modifierArticle.html.twig', [
+            'article' => $article,
+            'les_tags' => $this->lesTags(),
+        ]);
     }
 }
